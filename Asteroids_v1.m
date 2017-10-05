673ab9a87a2ccd1ad8c0a7527b4662ce246eaa59
clementinesm <bradsmith@utexas.edu>
2017-10-05 15:12:43 -0500

    MATLAB v.2
    Day 2; added simple Euler's method for computing pathlines, made 2D positions-array into 3D positions-array capable of holding time information about particles

--- Asteroids_v1.m
+++ Asteroids_v1.m
@@ -1,8 +1,10 @@
 clear;
 clc;
 
-n = 2; % Number of bodies
-G = 10; % Gravitational constant
+n = input('number of bodies = '); % Number of bodies
+G = 1; % Gravitational constant
+dt = 0.1;
+T = 10;
 
 for i = 1:n
     m(i) = input('mass = '); % Mass vector
@@ -13,20 +15,31 @@ for i = 1:n
     vy(i) = input('y-vel = '); % y-velocity vector
     vz(i) = 0; % z-velocity vector
 end
-x = [transpose(sx),transpose(sy),transpose(sz)]; % Combine x and y-positions
-v = [transpose(vx),transpose(vy),transpose(vz)]; % Combine x- and y-velocities
+x(:,:,1) = [transpose(sx),transpose(sy),transpose(sz)]; % Combine x and y-positions
+v_old = [transpose(vx),transpose(vy),transpose(vz)]; % Combine x- and y-velocities
 
-for i = 1:3 % Determines which dimension we're looking in (i = 1:x, i = 2:y, etc.)
-    for j = 1:n
-        for k = 1:n
-            if j == k
-                A(j,k) = 0; % i (dimension) comes last
-            else
-                A(j,k) = G * m(k) * (x(k,i) - x(j,i))/ ((x(j,1) - x(k,1))^2 + (x(j,2) - x(k,2))^2)^(3/2);
+for t = 1:(T/dt)
+    for i = 1:3 % Determines which dimension we're looking in (i = 1:x, i = 2:y, etc.)
+        for j = 1:n
+            for k = 1:n
+                if j == k
+                    A(j,k) = 0; % i (dimension) comes last
+                else
+                    A(j,k) = G * m(k) * (x(k,i,t) - x(j,i,t))/ ((x(j,1,t) - x(k,1,t))^2 + (x(j,2,t) - x(k,2,t))^2)^(3/2);
+                end
             end
         end
+    
+        a(:,i) = diag(A*ones(n));
     end
     
-    a(:,i) = diag(A*ones(n));
+    v_new = v_old + a * dt;
+    x(:,:,t+1) = x(:,:,t) + v_old * dt;
+    v_old = v_new;
 end
 
+for i = 1:n
+    plot_x(:,:) = x(i,:,:);
+    plot(plot_x(1,:),plot_x(2,:))
+    hold on
+end
